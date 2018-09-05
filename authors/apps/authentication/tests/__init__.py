785181bb7d3ee1f594110611b3a12c5669fc5078
cmplx-xyttmt <archimedesisaac@gmail.com>
2018-09-05 12:38:22 +0300

    feat(validation): Implement validation for signup
    - return descriptive error messages when user provides invalid data
    - refactor tests file into directory
    - write tests for validation
    - add maintainability badge
    
    [Delivers #159951962]
    
    Co-authored-by: Martha Nagginda <marthamareal@gmail.com>

---
+++ authors/apps/authentication/tests/__init__.py
@@ -0,0 +1,39 @@
+from ..models import User
+from rest_framework.test import APIClient
+from django.test import TestCase
+
+
+class BaseTest(TestCase):
+
+    def setUp(self):
+        self.user_data = {"user": {"username": "Jacob1",
+                                   "email": "jake@jakeg.jake",
+                                   "password": "jakejakeh1"}
+                          }
+
+        self.login_data_email = {"user": {"email": "jake@jakeg.jake",
+                                          "password": "jakejakeh1"}
+                                 }
+
+        self.unregistered_user_data = {"user": {"email": "jake@hhd.jake",
+                                                "password": "jakeakeh1"}
+                                       }
+
+        self.user_validate_data = {"user": {"username": "llll",
+                                            "email": "l@gmail.com",
+                                            "password": "p1234567"
+                                            }
+                                   }
+
+        User.objects.create_user("user_test", "mail@me.com", password="p12345678")
+        self.user_update_data = {"user": {"username": "user_test2",
+                                          "email": "mail@me.com",
+                                          "password": "p12345678"
+                                          }}
+
+        self.user = User.objects.get(email="mail@me.com")
+        self.client = APIClient()
+        self.register_response = self.client.post("/api/users/", self.user_data, format="json")
+        self.login_response = self.client.post("/api/users/login/", self.login_data_email, format="json")
+        token = self.login_response.data["token"]
+        self.client.credentials(HTTP_AUTHORIZATION=token)
