66eb9566e633cbe339474bb2cd62b44d00f89148
byarustev <byarus45@gmail.com>
2018-09-17 14:17:39 +0300

    feat(list users): implements returning of a list of users of the application
    - implement returning of registered users
    - implement test for this feature
    
    [finishes #159951975]

--- authors/apps/profiles/urls.py
+++ authors/apps/profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AuthentivatedProfileRetrieveAPIView, ProfileAPIView, FollowersAPIview
+from .views import AuthentivatedProfileRetrieveAPIView, ProfileAPIView, FollowersAPIView, UsersListAPIView
 
 urlpatterns = [
     path('profiles/<str:username>/', ProfileAPIView.as_view(), name='user_profile'),
     path('profiles/user/<str:username>/', AuthentivatedProfileRetrieveAPIView.as_view(), name='auth_user_profile'),
-    path('profiles/<str:username>/follow', FollowersAPIview.as_view(), name='follow'),
+    path('profiles/users', UsersListAPIView.as_view()),
+    path('profiles/<str:username>/follow', FollowersAPIView.as_view(), name='follow'),
 ]
